ceda792bfdd6a57e35571c8abf08da36e9c9e2b2
TomatoS0uP85 <faizabror85@student.uns.ac.id>
2023-12-31 16:26:13 +0700

    ver 1.0

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="Style_Home_Page.css" />
+        <title>Yamaca Automobile</title>
+    </head>
+<body>
+    <div id="wrap">
+        <div class="banner">
+            <div class="navbar">
+                <a href="Home_Page.html"><img src="image/Logo_Name.png" class="logo"></a>
+                <ul>
+                    <li><a href="Corporate.html">CORPORATE</a></li>
+                    <li><a href="Products.html">PRODUCTS</a></li>
+                    <li><a href="Service.html">SERVICE</a></li>
+                    <li><a href="Parts.html">PARTS</a></li>
+                    <li><a href="Racing.html">RACING</a></li>
+                </ul>
+            </div>
+
+            <div class="content">
+                <h1>YAMACA AUTOMOBILE</h1>
+                <a href="About_Us.html"><button type="button">about us</button></a>
+            </div>
+
+        </div>
+
+        <footer class="footer">
+            <p>&copy; 2023 PT. YAMACA Corp. All rights reserved. | <a href="Privacy Policy.html">Privacy Policy</a> | <a href="Terms of Service.html">Terms of Service</a></p>
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
